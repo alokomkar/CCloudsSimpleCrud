617e88b7dd8afa571d14a2ceb76995c29507cd08
alokomkar <alokomkar.gudikote@gmail.com>
2014-10-21 11:23:03 +0530

    AsyncTask Added

---
+++ src/com/example/database/handler/DBFetchTask.java
@@ -0,0 +1,50 @@
+package com.example.database.handler;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+
+import com.example.ccloudssimplecrud.MainActivity;
+
+public class DBFetchTask extends AsyncTask<Void, Void, Void>{
+
+	Context mContext;
+	DatabaseHandler mDatabaseHandler;
+	Cursor mCursor;
+	ProgressDialog mProgressDialog;
+	MainActivity mActivity;
+	
+	public DBFetchTask( MainActivity activity, DatabaseHandler databaseHandler ) {
+		this.mContext = activity;
+		this.mActivity = activity;
+		this.mProgressDialog = new ProgressDialog(mContext);
+		this.mDatabaseHandler = databaseHandler;
+	}
+	
+	@Override
+	protected void onPreExecute() {
+		mProgressDialog.setMessage("Refreshing Data, Please Wait...");
+		mProgressDialog.show();
+	}
+	
+	@Override
+	protected void onPostExecute(Void result) {
+		if( mProgressDialog.isShowing() ) {
+			mProgressDialog.dismiss();
+		}
+		mActivity.onAsyncTaskResult(mCursor);
+	}
+	
+	@Override
+	protected Void doInBackground(Void... params) {
+		
+		if( mDatabaseHandler == null ) {
+			mDatabaseHandler = new DatabaseHandler(mContext);
+		}
+		
+		mCursor = mDatabaseHandler.getAllDBTableCursor();
+		return null;
+	}
+
+}
